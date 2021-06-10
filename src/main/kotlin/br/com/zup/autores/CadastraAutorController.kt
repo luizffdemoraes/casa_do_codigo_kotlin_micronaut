db1e614982c5f121aff9430759801d84e092ce96
Luiz Moraes <lffm1994@gmail.com>
2021-06-10 09:49:05 -0300

    Definindo headers e status customizados

--- src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
@@ -1,9 +1,12 @@
 package br.com.zup.autores
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
+
 import javax.inject.Inject
 import javax.validation.Valid
 
@@ -11,8 +14,13 @@ import javax.validation.Valid
 @Controller("/autores")
 class CadastraAutorController(@Inject val autorRepository: AutorRepository) {
 
+   /*
+     HttpResponse é um tipo parametrizado <> é necessário informar qual e o tipo do corpo de resposta
+    - Ao informar o Any ele é analogo ao Object do Java, ao informar o any estamos dando um retorno
+    de corpo generico
+    */
     @Post
-    fun cadastra(@Body @Valid request: NovoAutorRequest){
+    fun cadastra(@Body @Valid request: NovoAutorRequest): HttpResponse<Any> {
 
         //request => domínio
 
@@ -22,6 +30,11 @@ class CadastraAutorController(@Inject val autorRepository: AutorRepository) {
         autorRepository.save(autor)
 
         println("Autor => ${autor.nome}")
+
+       val uri = UriBuilder.of("/autores/{id}")
+           .expand(mutableMapOf(Pair("id", autor.id)))
+
+       return HttpResponse.created(uri)
 
     }
 
